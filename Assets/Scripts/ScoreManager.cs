b9b5e169d77a61c237167fe0a84f027109ed815b
Gabs M. <gabmedinat@gmail.com>
2026-01-12 14:30:24 -0500

    Improving GameOver Scene + Quit system

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,38 +1,3 @@
-//using UnityEngine;
-//using TMPro;
-
-//public class ScoreManager : MonoBehaviour
-//{
-//    public static ScoreManager instance;
-//    [SerializeField] private TMP_Text scoreDisplay;
-
-//    public int score;
-
-//    private void Awake() {
-
-//        if (instance == null)
-//        {
-//            instance = this;
-//            DontDestroyOnLoad(gameObject);
-//        }
-//        else
-//        {
-//            Destroy(gameObject);
-//        }
-//    }
-
-//    private void OnGUI()
-//    {
-//        scoreDisplay.text = score.ToString();
-//    }
-
-//    public void updateScore(int amount)
-//    {
-//        score += amount;
-//    }
-//}
-
-
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
--- Assets/Scripts/WaterlevelHandler.cs
+++ Assets/Scripts/WaterlevelHandler.cs
@@ -9,10 +9,11 @@ public class WaterlevelHandler : MonoBehaviour
     private float WATER_MAX = -1.8f;
     private float WATER_MIN = -7.0f;
 
-    [SerializeField] private int maxScorePossible = 5000;
-    [SerializeField] private float smoothSpeed = 0.8f;
+    [Header("Game Settings")]
+    [SerializeField] private int maxScorePossible =1500;
+    [SerializeField] private float smoothSpeed = 1.2f; 
 
-    private bool Closing = false; 
+    private bool Closing = false;
 
     void Start()
     {
@@ -33,24 +34,36 @@ public class WaterlevelHandler : MonoBehaviour
         float targetY = Mathf.Lerp(WATER_MAX, WATER_MIN, t);
 
         float currentY = eau.transform.position.y;
-        float newY = Mathf.Lerp(currentY, targetY, Time.deltaTime * smoothSpeed);
+
+        float newY = Mathf.MoveTowards(currentY, targetY, smoothSpeed * Time.deltaTime);
 
         eau.transform.position = new Vector3(eau.transform.position.x, newY, eau.transform.position.z);
 
-        if (newY <= WATER_MIN + 0.05f && !Closing)
+        if (newY <= WATER_MIN + 1f && !Closing)
         {
             Closing = true;
             StartCoroutine(WaitAndClose());
         }
+
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
     }
 
     IEnumerator WaitAndClose()
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(30f);
         Application.Quit();
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
